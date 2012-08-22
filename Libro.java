f1acd93b2c3baf3cb8625f42b8a6ab89fcf52a7b
Leonardo Zanchi <zanchileonardo@gmail.com>
2012-08-22 17:52:16 +0200

    Aggiornato tutto

--- Libro.java
+++ Libro.java
@@ -7,50 +7,81 @@ public class Libro {
     private String anno;
     private String casaEditrice;    
     private String ISBN;
+    private String lingua;
+    private String voto;
 
-    public Libro(String id,String titolo,String autore,String anno,String casaEditrice,String ISBN) {
+    public Libro(String id,String titolo,String autore,String anno,String casaEditrice,String ISBN,String lingua,String voto) {
         this.id = id;
         this.titolo = titolo;
         this.autore = autore;
         this.anno = anno;
         this.casaEditrice = casaEditrice;
         this.ISBN = ISBN;
+        this.lingua = lingua;
+        this.voto = voto;
     }
         
     public void setID(String id) {
     	this.id = id;
     }
+    
     public void setTitolo(String titolo){
     	this.titolo = titolo;
     }
+    
     public void setAutore(String autore){
     	this.autore = autore;
     }
+    
     public void setAnno(String anno){
     	this.anno = anno;
     }
+    
     public void setCasaEditrice(String casaEditrice){
     	this.casaEditrice = casaEditrice;
     }
+    
     public void setISBN(String ISBN){
     	this.ISBN = ISBN;
-    }    
+    }
+    
+    public void setLingua(String lingua) {
+    	this.lingua = lingua;
+    }
+    
+    public void setVoto(String voto) {
+    	this.voto = voto;
+    }
+    
     public String getID() {
     	return id;
     }
+    
     public String getTitolo(){
     	return titolo;
     }
+    
     public String getAutore(){
     	return autore;
     }
+    
     public String getAnno(){
     	return anno;
     }
+    
     public String getCasaEditrice(){
     	return casaEditrice;
     }
+    
     public String getISBN(){
     	return ISBN;
+    }
+    
+    public String getLingua(){
+    	return lingua;
+    }
+    
+    public String getVoto(){
+    	return voto;
     }
 }
